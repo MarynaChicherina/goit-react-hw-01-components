4e6e390c121048a2cf63e7824857b5dcc1a9288c
MarynaChicherina <marinachicherina.ua@gmail.com>
2023-04-25 22:56:04 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,7 +18,6 @@ export const App = () => {
     tag={user.tag}
     location={user.location}
     avatar={user.avatar}
-    stats={user.stats}
     statsFollowers={user.stats.followers}
     statsLikes={user.stats.likes}
     statsViews={user.stats.views}
