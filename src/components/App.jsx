32ab5397bf7fa38826d4a722a8f410162616f6bc
MarynaChicherina <marinachicherina.ua@gmail.com>
2023-04-26 15:10:26 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,7 +29,7 @@ export const App = () => {
     statsViews={user.stats.views}
   />
   {title ? <Statistics title="Upload stats" stats={data} /> : <Statistics stats={data} />}
-  <FriendList friends={friends} />;
+  <FriendList friends={friends} />
     </div>
   );
 };
